9f2a0312251a83231b09653349fe0d124b4aad69
HenkeJohansson <henrik89johansson@gmail.com>
2020-05-06 11:04:16 +0200

    added script for configuring my displays at startup.

--- autostart.sh
+++ autostart.sh
@@ -9,6 +9,7 @@ function run {
 run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+run "xrandr --output DVI-D-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-4 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-5 --off --output HDMI-4 --off --output HDMI-5 --off --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off"
 run "nm-applet"
 #run "caffeine"
 run "pamac-tray"
---
+++ scripts/screen-layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-4 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-5 --off --output HDMI-4 --off --output HDMI-5 --off --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
