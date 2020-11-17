cf84c4497b6affaab79f084ee5176f90d227706d
HenkeJohansson <henrik89johansson@gmail.com>
2020-11-17 15:41:56 +0100

    Lazier navigation between screens.

--- rc.lua
+++ rc.lua
@@ -575,6 +575,8 @@ globalkeys = my_table.join(
               {description = "swap with next client by index", group = "client"}),
     awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
               {description = "swap with previous client by index", group = "client"}),
+    awful.key({ modkey}, "§", function () awful.screen.focus_relative( 1) end,
+              {description = "focus the next screen", group = "screen"}),
     awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
               {description = "focus the next screen", group = "screen"}),
     awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
