1784d657ded49681c2cb15260adc3a342fb87bcc
HenkeJohansson <henrik89johansson@gmail.com>
2021-06-22 19:31:47 +0200

    Fixed error in xrand script.

--- autostart.sh
+++ autostart.sh
@@ -29,7 +29,7 @@ run "volumeicon"
 #run "telegram-desktop"
 
 if [[ $COMPUTER_NAME == 'nuc10' ]]; then
-  xrandr --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 2570x0 --rotate normal --output HDMI-1 --off
+  xrandr --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-1 --off
 elif [[ $COMPUTER_NAME == 'workpc' ]]; then
   # run xrandr for workpc
   #run "franz"
